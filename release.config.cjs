7e538789b0ef4f5ef1fce6c053256c10f54991f4
julio.freitas <julio.freitas@solides.com.br>
2023-11-29 08:50:57 -0300

    feat(ci): update file pkg

--- release.config.cjs
+++ release.config.cjs
@@ -1,33 +1,38 @@
 module.exports = {
-  branches: [
-    '+([0-9])?(.{+([0-9]),x}).x',
-    'main',
-    {
-      name: 'beta',
-      prerelease: true,
-    },
-  ],
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/github',
-      {
-        assets: ['./**'],
-      },
+    branches: [
+        '+([0-9])?(.{+([0-9]),x}).x',
+        'main',
+        {
+            name: 'beta',
+            prerelease: true,
+        },
     ],
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'CHANGELOG.md',
-      },
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        [
+            '@semantic-release/github',
+            {
+                assets: ['./**'],
+            },
+        ],
+        [
+            '@semantic-release/changelog',
+            {
+                changelogFile: 'CHANGELOG.md',
+            },
+        ],
+        [
+            '@semantic-release/git',
+            {
+                assets: [
+                    'CHANGELOG.md',
+                    'package.json',
+                    'package-lock.json',
+                    '.',
+                ],
+                message: 'chore(release): ${nextRelease.version} [skip ci]}',
+            },
+        ],
     ],
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md', 'package.json', 'yarn-lock.json', '.'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]}',
-      },
-    ],
-  ],
 };
